2f999ab026c990a83ea39f305e149d2bf1d154c6
nurlancreus <nurlancreus007@gmail.com>
2025-01-24 18:01:53 +0400

    Add identity related service abstractions

---
+++ src/XpressShip.Application/Abstractions/Services/Session/IJwtSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XpressShip.Domain.Abstractions;
+
+namespace XpressShip.Application.Abstractions.Services.Session
+{
+    public interface IJwtSession
+    {
+        Result<bool> IsUserAuth();
+        Result<bool> IsSuperAdminAuth();
+        Result<bool> IsAdminAuth();
+        Result<bool> IsRolesAuth(IEnumerable<string> roleNames);
+        Result<IEnumerable<string>> GetRoles();
+        Result<string> GetUserId();
+        Result<string> GetUserName();
+        Result<string> GetUserEmail();
+
+    }
+}
---
+++ src/XpressShip.Application/Abstractions/Services/Token/ITokenService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using XpressShip.Application.DTOs.Token;
+using XpressShip.Domain.Abstractions;
+using XpressShip.Domain.Entities.Users;
+
+namespace XpressShip.Application.Abstractions.Services.Token
+{
+    public interface ITokenService
+    {
+        Task<Result<TokenDTO>> CreateAccessTokenAsync(ApplicationUser user);
+        Task<Result<string>> CreateRefreshTokenAsync();
+        Result<ClaimsPrincipal> GetPrincipalFromAccessToken(string? accessToken);
+    }
+}
