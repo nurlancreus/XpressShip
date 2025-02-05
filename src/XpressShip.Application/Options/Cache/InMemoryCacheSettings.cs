1fd3bbf8966b66cc79ec196368848a49ccb8a14f
nurlancreus <nurlancreus007@gmail.com>
2025-02-05 15:37:49 +0400

    Add options settings for caching

---
+++ src/XpressShip.Application/Options/Cache/InMemoryCacheSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XpressShip.Application.Options.Cache
+{
+    public class InMemoryCacheSettings
+    {
+        public const string CountryData = "CountryData";
+
+        public int SlidingExpirationInHours { get; set; }
+        public int AbsoluteExpirationInHours { get; set; }
+        public string CacheKey { get; set; } = string.Empty;
+    }
+}
