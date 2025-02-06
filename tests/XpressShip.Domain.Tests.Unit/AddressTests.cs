ef07b29dd957661acb7c28c6a5ec6a5c56aca6db
nurlancreus <nurlancreus007@gmail.com>
2025-02-06 18:31:54 +0400

    Add Address and ApiClient tests

---
+++ tests/XpressShip.Domain.Tests.Unit/AddressTests.cs
@@ -0,0 +1,99 @@
+﻿using FluentAssertions;
+using XpressShip.Domain.Entities;
+using XpressShip.Tests.Common.Factories;
+
+namespace XpressShip.Domain.Tests.Unit
+{
+    public class AddressTests
+    {
+        [Fact]
+        public void CalculateDistance_WhenProvidingValidAddress_ShouldCalculateDistance()
+        {
+            // Arrange
+            var origin = Factory.Address.GenerateOriginAddress();
+            var destination = Factory.Address.GenerateDestinationAddress();
+
+            // Act
+            var answer = origin.CalculateDistance(destination);
+
+            // Assert
+            answer.Should().NotBe(0);
+        }
+
+        [Theory]
+        [InlineData(90)]
+        [InlineData(-90)]
+        [InlineData(0)]
+        public void IsValidLatitude_WhenProvidingValidLat_ShouldReturnTrue(double latitude)
+        {
+            // Arrange & Act
+            var answer = Address.IsValidLatitude(latitude);
+
+            // Assert
+            answer.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(91)]
+        [InlineData(-91)]
+        [InlineData(100)]
+        public void IsValidLatitude_WhenProvidingInValidLat_ShouldThrowException(double latitude)
+        {
+            // Arrange & Act
+            var action = () => Address.IsValidLatitude(latitude);
+
+            // Assert
+            action.Should().ThrowExactly<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData(91)]
+        [InlineData(-91)]
+        [InlineData(100)]
+        public void IsValidLatitude_WhenProvidingInValidLatAndThrowExceptionIsFalse_ShouldReturnFalse(double latitude)
+        {
+            // Arrange & Act
+            var answer = Address.IsValidLatitude(latitude, false);
+
+            // Assert
+            answer.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(180)]
+        [InlineData(-180)]
+        [InlineData(0)]
+        public void IsValidLongitude_WhenProvidingValidLon_ShouldReturnTrue(double longitude)
+        {
+            // Arrange & Act
+            var answer = Address.IsValidLongitude(longitude);
+
+            // Assert
+            answer.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(181)]
+        [InlineData(-181)]
+        public void IsValidLongitude_WhenProvidingInValidLon_ShouldThrowException(double longitude)
+        {
+            // Arrange & Act
+            var action = () => Address.IsValidLongitude(longitude);
+
+            // Assert
+            action.Should().ThrowExactly<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData(181)]
+        [InlineData(-181)]
+        public void IsValidLongitude_WhenProvidingInValidLonAndThrowExceptionIsFalse_ShouldReturnFalse(double longitude)
+        {
+            // Arrange & Act
+            var answer = Address.IsValidLongitude(longitude, false);
+
+            // Assert
+            answer.Should().BeFalse();
+        }
+    }
+}
---
+++ tests/XpressShip.Domain.Tests.Unit/ApiClientTests.cs
@@ -0,0 +1,114 @@
+﻿using FluentAssertions;
+using XpressShip.Domain.Entities;
+using XpressShip.Tests.Common.Factories;
+using DataConstants = XpressShip.Tests.Common.Constants.Constants;
+
+namespace XpressShip.Domain.Tests.Unit
+{
+    public class ApiClientTests
+    {
+        [Fact]
+        public void Create_ShouldInitializeWithRawSecretKey()
+        {
+            // Arrange
+            var companyName = DataConstants.ApiClient.CompanyName;
+            var email = DataConstants.ApiClient.Email;
+
+            // Act
+            var (client, rawSecretKey) = ApiClient.Create(companyName, email);
+
+            // Assert
+            client.Should().NotBeNull();
+            rawSecretKey.Should().NotBeEmpty();
+            client.CompanyName.Should().Be(companyName);
+            client.Email.Should().Be(email);
+            client.ApiKey.Should().NotBeEmpty();
+            client.SecretKey.Should().NotBeEmpty();
+            client.IsActive.Should().BeFalse();
+        }
+
+        [Fact]
+        public void VerifySecretKey_WhenRawSecretAndHashedIsEqual_ShouldReturnTrue()
+        {
+            // Arrange
+            var (client, rawSecretKey) = Factory.ApiClient.GenerateApiClient();
+
+            // Act
+            var answer = ApiClient.VerifySecretKey(rawSecretKey, client.SecretKey);
+
+            // Assert
+            answer.Should().BeTrue();
+        }
+
+        [Fact]
+        public void VerifySecretKey_WhenRawSecretAndHashedIsNotEqual_ShouldReturnFalse()
+        {
+            // Arrange
+            var (client, _) = Factory.ApiClient.GenerateApiClient();
+
+            // Act
+            var answer = ApiClient.VerifySecretKey(string.Empty, client.SecretKey);
+
+            // Assert
+            answer.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Toggle_WhenIsActiveTrue_ShouldToggleAndSetDeActivatedAtNull()
+        {
+            // Arrange
+            var (client, _) = Factory.ApiClient.GenerateApiClient();
+
+            // Act
+            client.Toggle();
+
+            // Assert
+            client.IsActive.Should().BeTrue();
+            client.DeActivatedAt.Should().BeNull();
+        }
+
+        [Fact]
+        public void Toggle_WhenIsActiveFalse_ShouldToggleAndSetDeActivatedAtNotNull()
+        {
+            // Arrange
+            var (client, _) = Factory.ApiClient.GenerateApiClient();
+            client.IsActive = true;
+
+            // Act
+            client.Toggle();
+
+            // Assert
+            client.IsActive.Should().BeFalse();
+            client.DeActivatedAt.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void UpdateApiKey_WhenUpdateApiKey_ShouldChange()
+        {
+            // Arrange
+            var (client, _) = Factory.ApiClient.GenerateApiClient();
+            var oldApiKey = client.ApiKey;
+
+            // Act
+            client.UpdateApiKey();
+
+            // Assert
+            oldApiKey.Should().NotBeEquivalentTo(client.ApiKey);
+        }
+
+        [Fact]
+        public void UpdateSecretKey_WhenUpdateSecretKey_ShouldChange()
+        {
+            // Arrange
+            var (client, oldRawSecretKey) = Factory.ApiClient.GenerateApiClient();
+            var oldSecretKey = client.SecretKey;
+
+            // Act
+            var newRawSecretKey = client.UpdateSecretKey();
+
+            // Assert
+            oldRawSecretKey.Should().NotBeEquivalentTo(newRawSecretKey);
+            oldSecretKey.Should().NotBeEquivalentTo(client.SecretKey);
+        }
+    }
+}
